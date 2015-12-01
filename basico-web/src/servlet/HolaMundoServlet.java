acbf61429a09da18c2cb9cbd91de03c5a836c54d
Rodolfo Campos <camposer@gmail.com>
2015-12-01 10:19:36 +0100

    Ejemplo básico de requests

--- basico-web/src/servlet/HolaMundoServlet.java
+++ basico-web/src/servlet/HolaMundoServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,17 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet({ "/HolaMundoServlet", "/hola-mundo" })
 public class HolaMundoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-      
+
 	/**
-	 * Método que se va a ejecutar cuando la petición venga por GET
-	 * Para ejecutar:
-	 * 1.- Ir a la vista Servers -> Start Server
-	 * 2.- Abrir en el navegador la siguiente URL:
-	 * 	http://localhost:8080/basico-web/hola-mundo
-	 * 	http://localhost:8080/basico-web/HolaMundoServlet
+	 * Método que se va a ejecutar cuando la petición venga por GET Para
+	 * ejecutar: 
+	 * 1.- Ir a la vista Servers -> Start Server 
+	 * 2.- Abrir en el navegador la siguiente URL: 
+	 * http://localhost:8080/basico-web/
+	 * 	hola-mundo?nombre=Juan&apellido=Jaimes
+	 * http://localhost:8080/basico-web/HolaMundoServlet
 	 */
-	protected void doGet(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String nombre = request.getParameter("nombre");
+		String apellido = request.getParameter("apellido");
 
 		PrintWriter pw = response.getWriter();
 		pw.println("<html>");
@@ -30,7 +35,18 @@ public class HolaMundoServlet extends HttpServlet {
 		pw.println("<title>Hola mundo</title>");
 		pw.println("</head>");
 		pw.println("<body>");
-		pw.println("Hola mundo");
+		pw.println("Hoy es " + new Date() + "<br>");
+
+		if (nombre != null && apellido != null) {
+			pw.println("Hola " + nombre + " " + apellido + "<br>");
+		} else {
+			pw.println("<form>");
+			pw.println("Nombre: <input type='text' name='nombre'><br>");
+			pw.println("Apellido: <input type='text' name='apellido'><br>");
+			pw.println("<input type='submit' value='enviar'><br>");
+			pw.println("</form>");
+		}
+		
 		pw.println("</body>");
 		pw.println("</html>");
 	}
